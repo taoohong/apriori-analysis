1a55a1c8d68bbd186ceb73a63df7c45bea60b1b9
taohong <ht1135@foxmail.com>
2024-05-16 13:06:32 +0800

    done
    Signed-off-by: taohong <ht1135@foxmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import sqlite3
+import sys
 import pandas as pd
 from flask import Flask, render_template, jsonify, request
 from mlxtend.preprocessing import TransactionEncoder
@@ -12,8 +13,8 @@ app.config.from_object('config')
 
 login_name = None
 
-order_df = pd.read_csv('./data/GoodsOrder.csv', encoding='gbk')
-type_df = pd.read_csv('./data/GoodsTypes.csv', encoding='gbk')
+order_df = pd.read_csv('./data/GoodsOrder.csv', encoding='utf-8')
+type_df = pd.read_csv('./data/GoodsTypes.csv', encoding='utf-8')
 
 order_group = order_df.groupby(by='id')
 
@@ -114,7 +115,7 @@ def login(name, password):
 def type_vis():
     """婴儿性别和年龄数据"""
     type_counts = type_df['Types'].value_counts().reset_index()
-    type_counts = type_counts.sort_values(by='index')
+    type_counts = type_counts.sort_values(by='count')
 
     # 用户一次购买商品数量分布
     product_len_counts = {}
@@ -136,8 +137,8 @@ def type_vis():
     product_counts = sorted(product_counts.items(), key=lambda x: x[1], reverse=True)
 
     return jsonify({
-        '类别': [g for g in type_counts['index'].values],
-        '商品个数': type_counts['Types'].values.tolist(),
+        '类别': [g for g in type_counts['Types'].values],
+        '商品个数': type_counts['count'].values.tolist(),
         '购买商品个数': [pc[0] for pc in product_len_counts],
         '购买商品个数人次': [pc[1] for pc in product_len_counts],
         '商品': [pc[0] for pc in product_counts],
@@ -159,14 +160,30 @@ def clac_product_association(min_support, min_threshold):
     # 关联结果按照置信度或提升度高进行排序
     result = result.sort_values(by='confidence', ascending=False)
     # 结果保存
-    result.to_csv('./results/product_association_min_support{}_{}.csv'.format(min_support, min_threshold),
-                  encoding='utf8', index=False)
+    # result.to_csv('./results/product_association_min_support{}_{}.csv'.format(min_support, min_threshold),
+    #               encoding='utf8', index=False)
 
     results = result.to_dict(orient='records')
     for result in results:
         result['antecedents'] = str(result['antecedents'])[10: -1]  # 前因
         result['consequents'] = str(result['consequents'])[10: -1]  # 后果
 
+    return jsonify(results)
+
+
+@app.route('/calc_association/<item1>/<item2>')
+def calc_association(item1, item2):
+    """计算某两个产品之间的关联规则"""
+    te = TransactionEncoder()
+    te_ary = te.fit(products).transform(products)
+    df = pd.DataFrame(te_ary, columns=te.columns_)
+    freq = apriori(df, min_support=float(0.01), use_colnames=True)
+    result = association_rules(freq, metric="confidence", min_threshold=float(0))
+    result['antecedents'] = result['antecedents'].apply(lambda x: ', '.join(sorted(list(x))))
+    result['consequents'] = result['consequents'].apply(lambda x: ', '.join(sorted(list(x))))
+    rule = result[(result['antecedents'] == item1) & (result['consequents'] == item2)]
+    print(rule)
+    results = rule.to_dict(orient='records')
     return jsonify(results)
 
 
@@ -184,4 +201,4 @@ def query_products():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1')
+    app.run(host='127.0.0.1', port=50000)
